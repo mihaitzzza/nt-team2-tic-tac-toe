5c89825ab5e1fca3f2a9f9c3c04db21592f710b8
_ciurez_bogdan <ciurezbogdan@hotmail.com>
2021-02-16 18:34:38 +0200

    Version 2 authentication

--- game/player.py
+++ game/player.py
@@ -1,14 +1,16 @@
-def get_current_player(step):
+def get_current_player(step, pl1, pl2):
     """
     This function is used for getting data about the current player.
-    :param step: Integer about the current iteration.
+    :param step: Integer about the current iteration,
+    :param pl1 str for player_1 name,
+    :param pl2 for player_2 name
     :return: (player_name, player_sign)
     """
     if step % 2 == 0:
-        name = 'Player 1'
+        name = pl1
         sign = 'x'
     else:
-        name = 'Player 2'
+        name = pl2
         sign = 'o'
 
     return name, sign
